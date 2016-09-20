0c3c29886f68299fd9bed15978a67e175f43a05e
subhash kumar singh <subhash.kumar.singh@huawei.com>
2016-09-20 16:13:36 +0530

    Fix convention to use LOGGER
    Fix the convention to use the LOGGER variable(without using the
    classname) eg. LOGGER.info("foo");
    
    Correct the import ordering.
    
    Add annotation for the overridden methods.
    
    Change-Id: I52a87941c2da0a3fb5588e119309dd433b778f3a
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

--- service/src/main/java/org/openo/sdno/acwanservice/impl/L3vpnAcService.java
+++ service/src/main/java/org/openo/sdno/acwanservice/impl/L3vpnAcService.java
@@ -20,8 +20,6 @@ import java.text.MessageFormat;
 import java.util.Map;
 
 import org.openo.baseservice.remoteservice.exception.ServiceException;
-import org.openo.sdno.wanservice.constants.L3VpnSvcErrorCode;
-import org.openo.sdno.wanservice.inf.Il3VpnService;
 import org.openo.sdno.acwanservice.transformer.NetToSerTransformer;
 import org.openo.sdno.acwanservice.transformer.SerToNetTransformer;
 import org.openo.sdno.common.restconf.ContentType;
@@ -37,6 +35,8 @@ import org.openo.sdno.model.networkmodel.servicetypes.L3VpnConfig;
 import org.openo.sdno.model.networkmodel.servicetypes.VpnOperStatus;
 import org.openo.sdno.result.Result;
 import org.openo.sdno.util.http.HTTPReturnMessage;
+import org.openo.sdno.wanservice.constants.L3VpnSvcErrorCode;
+import org.openo.sdno.wanservice.inf.Il3VpnService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -65,6 +65,7 @@ public class L3vpnAcService implements Il3VpnService {
      * @return
      * @since SDNO 0.5
      */
+    @Override
     public Result<String> l3vpnStatusUpdate(String request, String vpdId, String ctrlUuid) throws ServiceException {
 
         final org.openo.sdno.model.uniformsbi.l3vpn.L3Vpn l3Vpn =
@@ -72,7 +73,7 @@ public class L3vpnAcService implements Il3VpnService {
 
         L3VpnConfig ctrlrl3vpn = SerToNetTransformer.transformModel(l3Vpn);
         final String l3vpnMsg = SerializeUtil.serialize(L3vpnAcService.CONTENT_TYPE, ctrlrl3vpn);
-        L3vpnAcService.LOGGER.info(l3vpnMsg);
+        LOGGER.info(l3vpnMsg);
 
         final String url =
                 MessageFormat.format("/restconf/config/huawei-ac-net-l3vpn:l3vpn-cfg/instances/instance/{0}", vpdId);
@@ -81,7 +82,7 @@ public class L3vpnAcService implements Il3VpnService {
             return new Result<>(ErrorCode.OPERATION_SUCCESS, msg.getBody());
         }
 
-        L3vpnAcService.LOGGER.error("adminStatusUpdate failed:" + msg.getBody());
+        LOGGER.error("adminStatusUpdate failed:" + msg.getBody());
         throw ServiceExceptionUtil.getServiceException(L3VpnSvcErrorCode.L3VPN_UPDATE_STATUS_CONTROLLER_FAIL,
                 msg.getStatus(), msg.getBody(), ctrlUuid);
     }
@@ -94,6 +95,7 @@ public class L3vpnAcService implements Il3VpnService {
      * @return The result of creating L3vpn
      * @since SDNO 0.5
      */
+    @Override
     public Result<String> l3vpnCreate(final String spi, String ctrlUuid) throws ServiceException {
         final String url = "/restconf/config/huawei-ac-net-l3vpn:l3vpn-cfg/instances";
 
@@ -102,12 +104,12 @@ public class L3vpnAcService implements Il3VpnService {
 
         L3VpnConfig ctrlrl3vpn = SerToNetTransformer.transformModel(l3Vpn);
         final String l3vpnMsg = SerializeUtil.serialize(L3vpnAcService.CONTENT_TYPE, ctrlrl3vpn);
-        L3vpnAcService.LOGGER.info(l3vpnMsg);
+        LOGGER.info(l3vpnMsg);
         final HTTPReturnMessage msg = RestConfProxy.post(L3vpnAcService.CONTENT_TYPE, url, ctrlUuid, l3vpnMsg);
         if(HttpCode.isSucess(msg.getStatus())) {
             return new Result<>(ErrorCode.OPERATION_SUCCESS, msg.getBody());
         } else {
-            L3vpnAcService.LOGGER.error("l3vpnCreate failed:" + msg.getBody());
+            LOGGER.error("l3vpnCreate failed:" + msg.getBody());
             throw ServiceExceptionUtil.getServiceException(L3VpnSvcErrorCode.L3VPN_CREATE_CONTROLLER_FAIL,
                     msg.getStatus(), msg.getBody(), ctrlUuid);
         }
@@ -121,13 +123,14 @@ public class L3vpnAcService implements Il3VpnService {
      * @return The result of deleting
      * @since SDNO 0.5
      */
+    @Override
     public Result<String> l3vpnDelete(String ctrlId, String vpnId) throws ServiceException {
         final String url = "/restconf/config/huawei-ac-net-l3vpn:l3vpn-cfg/instances/instance/" + vpnId;
         final HTTPReturnMessage msg = RestConfProxy.del(L3vpnAcService.CONTENT_TYPE, url, ctrlId);
         if(HttpCode.isSucess(msg.getStatus())) {
             return new Result<>(ErrorCode.OPERATION_SUCCESS, msg.getBody());
         } else {
-            L3vpnAcService.LOGGER.error("l3vpnDelete failed:" + msg.getBody());
+            LOGGER.error("l3vpnDelete failed:" + msg.getBody());
             throw ServiceExceptionUtil.getServiceException(L3VpnSvcErrorCode.L3VPN_DELETE_CONTROLLER_FAIL,
                     msg.getStatus(), msg.getBody(), ctrlId);
         }
@@ -140,16 +143,17 @@ public class L3vpnAcService implements Il3VpnService {
      * @return The result of updating
      * @since SDNO 0.5
      */
+    @Override
     public Result<L3Vpn> l3vpnUpdateDecs(final ServiceParasInfo spi) throws ServiceException {
         final L3Vpn l3Vpn = JsonUtil.fromJson(spi.getServiceBody(), L3Vpn.class);
-        L3vpnAcService.LOGGER.info("l3vpn update :" + spi.getServiceBody());
+        LOGGER.info("l3vpn update :" + spi.getServiceBody());
         final String url = "/restconf/config/huawei-ac-net-l3vpn:l3vpn-cfg/instances/instance/" + l3Vpn.getId();
         final HTTPReturnMessage msg = RestConfProxy.put(L3vpnAcService.CONTENT_TYPE, url, spi.getCltuuid(),
                 SerializeUtil.serialize(L3vpnAcService.CONTENT_TYPE, l3Vpn));
         if(HttpCode.isSucess(msg.getStatus())) {
             return new Result<>(ErrorCode.OPERATION_SUCCESS, l3Vpn);
         } else {
-            L3vpnAcService.LOGGER.error("l3vpnUpdate failed:" + msg.getBody());
+            LOGGER.error("l3vpnUpdate failed:" + msg.getBody());
             throw ServiceExceptionUtil.getServiceException(L3VpnSvcErrorCode.UPDATEDESC_CONTROLLER_FAIL,
                     msg.getStatus(), msg.getBody(), spi.getCltuuid());
         }
@@ -162,6 +166,7 @@ public class L3vpnAcService implements Il3VpnService {
      * @return The result of getting
      * @since SDNO 0.5
      */
+    @Override
     public Result<VpnOperStatus> l3vpnOperStatusGet(final ServiceParasInfo spi) throws ServiceException {
         final Map<String, String[]> obj = spi.getQueryMap();
         final String uuid = obj.get("uuid")[0];
@@ -170,10 +175,10 @@ public class L3vpnAcService implements Il3VpnService {
         if(HttpCode.isSucess(msg.getStatus())) {
             final VpnOperStatus operStatus =
                     SerializeUtil.deSerialize(L3vpnAcService.CONTENT_TYPE, msg.getBody(), VpnOperStatus.class);
-            L3vpnAcService.LOGGER.info("doGetL3vpnCfg success:" + msg.getBody());
+            LOGGER.info("doGetL3vpnCfg success:" + msg.getBody());
             return new Result<>(ErrorCode.OPERATION_SUCCESS, operStatus == null ? null : operStatus);
         } else {
-            L3vpnAcService.LOGGER.error("doGetL3vpn failed:" + msg.getBody());
+            LOGGER.error("doGetL3vpn failed:" + msg.getBody());
             throw ServiceExceptionUtil.getServiceException(L3VpnSvcErrorCode.L3VPN_GET_STATUS_CONTROLLER_FAIL,
                     msg.getStatus(), msg.getBody(), spi.getCltuuid());
         }
@@ -187,6 +192,7 @@ public class L3vpnAcService implements Il3VpnService {
      * @return The result of getting L3vpn information
      * @since SDNO 0.5
      */
+    @Override
     public Result<String> l3vpnGet(String ctrlUuid, String vpnId) throws ServiceException {
 
         final String url = "/restconf/config/huawei-ac-net-l3vpn:l3vpn-cfg/instances/instance/" + vpnId;
@@ -195,10 +201,10 @@ public class L3vpnAcService implements Il3VpnService {
             String str = msg.getBody().replaceAll("\\\\", "");
             final L3VpnConfig l3Vpn = SerializeUtil.deSerialize(L3vpnAcService.CONTENT_TYPE, str, L3VpnConfig.class);
             org.openo.sdno.model.uniformsbi.l3vpn.L3Vpn nbiL3vpn = NetToSerTransformer.transformModel(l3Vpn);
-            L3vpnAcService.LOGGER.info("doGetL3vpnOperStatus success:" + msg.getBody());
+            LOGGER.info("doGetL3vpnOperStatus success:" + msg.getBody());
             return new Result<>(ErrorCode.OPERATION_SUCCESS, JsonUtil.toJson(nbiL3vpn));
         } else {
-            L3vpnAcService.LOGGER.error("doGetL3vpn failed:" + msg.getBody());
+            LOGGER.error("doGetL3vpn failed:" + msg.getBody());
             throw ServiceExceptionUtil.getServiceException(L3VpnSvcErrorCode.L3VPN_GET_CONTROLLER_FAIL, msg.getStatus(),
                     msg.getBody(), ctrlUuid);
         }
