9920ecd1bc76f450042aef5ea7ecc11fc9b79da2
angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
2016-09-22 17:17:24 +0800

    Fix Service Start and Stop Script.
    Change-Id: If388e44e6b74cfd20f09ff37936356efdb1fdc0f
    Signed-off-by: angilecoder <zhaoyangzhao.zhaoyang@huawei.com>

--- deployment/src/main/release/bin/start.sh
+++ deployment/src/main/release/bin/start.sh
@@ -1,52 +1,19 @@
-#!/bin/bash
+#*******************************************************************************
 # Copyright 2016 Huawei Technologies Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-
-if [ -z "$JAVA_HOME" ]
-then
-    echo "There is no JAVA_HOME"
-    exit 1
-fi
-
-if [ -z "$CATALINA_HOME" ]
-then
-    echo "There is no CATALINA_HOME"
-    exit 1
-fi
-
+#*******************************************************************************
+#!/bin/bash
 cd ..
 export CATALINA_BASE=$(cd `dirname $0`; pwd)
-
-if [ -z "$CATALINA_BASE" ]
-then
-    echo "There is no CATALINA_BASE"
-    exit 1
-fi
-
-LOG_DIR=$CATALINA_BASE/logs
-if [ ! -d "$LOG_DIR" ]; then
-  mkdir $LOG_DIR
-fi
-
-ACCESS_GRP=$OPENO_HOME/groups
-if [ ! -d "$ACCESS_GRP" ];
-then
-    mkdir $ACCESS_GRP
-fi
-
-
-$CATALINA_HOME/bin/catalina.sh start
-
-
+catalina.sh start
